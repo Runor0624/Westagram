a19b9da029c3a34ad6e02e6d8dd9a8ad322992b7
한상일 <mint@hansang-il-ui-MacBookAir.local>
2022-04-17 21:35:12 +0900

    로그인 회원가입 레이아웃 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Text>123</Text>
+      <Footer />
     </div>
-  );
+  )
 }
 
 const Text = styled.p`
-font-weight: bold;
-`;
+  font-weight: bold;
+`
 
-export default App;
+export default App
--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Login from './Pages/Login/Login'
+import SignUp from './Pages/SignUp/SignUp'
 
 function Router() {
-    return (
-<BrowserRouter>
-<Routes>
-    <Route path="/" element={<App />} />
-</Routes>
-</BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-export default Router;
+export default Router
