d86eff7b9f2a0fd89f880534fc5313b2bc195f4f
한상일 <mint@hansang-il-ui-MacBookAir.local>
2022-04-18 22:30:18 +0900

    Nav 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import Footer from './Components/Footer/Footer'
+import Nav from './Components/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Text>123</Text>
       <Footer />
     </div>
